cc8c0e4772f93aaf93d2884bc013a685bc7327fb
congdp <congdinhpham99@gmail.com>
2021-04-12 18:47:50 +0700

    fontEndWeb

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="header my-3">
+      <web-header />
+    </div>
+    <div class="row">
+      <web-menu/>
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+import WebHeader from '../components/WebHeader'
+import WebMenu from '../components/WebMenu'
+export default {
+  components: {
+    WebHeader,
+    WebMenu
+   
+  },
+}
+</script>
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="col-lg-9" style="border-left:1px solid #bdbdbd" >
+    <list-product :dataProducts="dataProducts" @getListBlogs="getData" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import {URL_API} from '../constants/constants'
+import ListProduct from '../components/Products/ListProduct'
+export default {
+  components: {
+    ListProduct
+  },
+  data() {
+    return {
+      dataProducts: [],
+    }
+  },
+  methods: {
+    /**
+     * get list product
+     */
+    getData() {
+      axios.get(URL_API + 'product').then((res) => {
+        this.dataProducts = res.data.data
+        console.log(this.dataProducts)
+      })
+    },
+  },
+  mounted() {
+    this.getData()
+  },
+}
+</script>
